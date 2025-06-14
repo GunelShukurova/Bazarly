ab2b854cae81f3e520c51f3a461b423ab76e29fc
Gunel Shukurova <gunelnsh@code.edu.az>
2025-06-14 17:18:15 +0400

    home page functionality completed

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -16,19 +16,24 @@ const Slider = () => {
         }}
         navigation={true}
         modules={[Navigation]}
-        className="mySwiper w-80">
+        className="mySwiper w-screen">
 
         <SwiperSlide>
-          <img style={{ width: "100%", height: "70vh" }} src="https://cdn.pixabay.com/photo/2020/11/21/19/43/watch-5764987_1280.jpg" alt="" />
+           <div className="relative">
+          <img  className="w-screen h-screen object-cover" src="https://cdn.pixabay.com/photo/2020/11/21/19/43/watch-5764987_1280.jpg" alt="" />
 
-          <div className='bg-pink-50 '>
-
-            <h1 className='absolute top-50 left-105 text-8xl  text-[#F8F6F0] text-center p-3 rounded-2xl'>Welcome to Bazarly</h1>
-            <p className='absolute top-75 left-40 text-2xl  text-[#F8F6F0] text-center p-3 rounded-2xl px-50'>Your one-stop online marketplace for everyday essentials. From beverages to hygiene products, we've got everything you need.</p>
-            <button className='absolute left-168 top-95 mt-5 bg-white border border-black px-14 py-3 cursor-pointer hover:bg-gray-100'>Shop Now</button>
-            <button className='absolute top-95 mt-5   left-220  bg-white border border-black px-14 py-3 cursor-pointer hover:bg-gray-100'>Learn More</button>
-          </div>
-          
+ <div className="absolute inset-0  bg-opacity-50 z-10 !bg-black/50" />
+          <div className="absolute inset-0 z-20 flex flex-col items-center justify-center text-center text-white px-8">
+      <h1 className="text-7xl font-bold mb-4">Welcome to Bazarly</h1>
+      <p className="text-xl max-w-2xl mb-6">
+        Your one-stop online marketplace for everyday essentials. From beverages to hygiene products, we've got everything you need.
+      </p>
+      <div className="flex gap-6">
+        <button className="bg-[#292417ce] text-white px-6 py-3 font-semibold hover:bg-[#292723ce] cursor-pointer border border-black">Shop Now</button>
+        <button className="bg-[#292417ce] text-white px-6 py-3 font-semibold hover:bg-[#292723ce] cursor-pointer border border-black">Learn More</button>
+      </div>
+        </div>
+    </div>
         </SwiperSlide >
 
         <SwiperSlide>
--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -1,27 +1,164 @@
 import Slider from "../../components/Slider/index.jsx"
 import { SlBasket } from "react-icons/sl";
-import { FaCar } from "react-icons/fa6";
+import { GoStar } from "react-icons/go";
 import { FaStar } from "react-icons/fa";
 import { IoPricetagsOutline } from "react-icons/io5";
 import { FiShield } from "react-icons/fi";
 import { IoMdTime } from "react-icons/io";
 
+import { useEffect, useState } from "react";
+import { getAllProducts } from "../../services/products/requests.js";
+
 
 const Home = () => {
+
+
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+
+    getAllProducts().then((resp) => {
+      if (resp) {
+        setProducts(resp.data)
+      }
+    })
+
+  }, [])
+
+const filteredProducts = products.filter((p) =>
+  p.title.toLowerCase().includes(search.toLowerCase()) ||
+  p.category.toLowerCase().includes(search.toLowerCase()) ||
+  p.description.toLowerCase().includes(search.toLowerCase())
+);
+
   return (
     <div>
       <Slider />
+
+      <div className="w-full max-w-3xl  mt-20 ml-30 flex justify-start items-center gap-4 bg-[#F8F6F0] p-3  shadow-sm border border-gray-200">
+        <input
+  value={search}
+  onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Search products..."
+          className="px-3 py-2 border border-gray-300 w-200 text-lg rounded-md focus:outline-none  focus:ring-1 focus:ring-gray-500"
+        />
+      </div>
+
+
+
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 mx-30 mt-15 bg-[#FDFBF6]">
+
+        {filteredProducts.length ? (
+          filteredProducts.map((p) => (
+            <div key={p.id} className="max-w-lg  shadow-md overflow-hidden p-4 bg-[#F8F6F0] cursor-pointer relative group">
+
+              <div className='flex flex-col justify-center items-center relative'>
+
+
+                <h3 className="text-xl text-center text-shadow-neutral-600 font-normal mb-2 mt-2 ">
+                  {p.title}
+                </h3>
+
+
+
+                <img
+                  className="w-75 h-65 my-17 object-cover rounded"
+                  src={p.image}
+                  alt={p.title}
+                />
+
+
+                <div className="mb-4">
+
+                  <p className="text-lg text-center text-neutral-600 mb-2">
+                    {p.description}
+                  </p>
+                  <span className="block font-normal text-xl text-center mt-3">
+                    {p.category}
+                  </span>
+                </div>
+                <div className="flex items-center justify-between text-sm text-gray-700">
+
+
+
+                </div>
+
+              </div>
+              <div className="flex justify-center items-center flex-col mb-2">
+                {p.isOnSale ? (
+                  <>
+                    <span className="absolute top-6 left-5 bg-red-800 text-white px-3 py-1 text-sm font-semibold rounded">
+                      -{p.salePercentage}%
+                    </span>
+                    <span className="text-xl text-gray-500 line-through mb-1">${p.price.toFixed(2)}</span>
+                    <span className="text-2xl font-bold text-gray-800">
+                      ${(p.price * (1 - p.salePercentage / 100)).toFixed(2)}
+                    </span>
+                  </>
+                ) : (
+                  <span className="text-2xl font-bold text-black">${p.price.toFixed(2)}</span>
+                )}
+              </div>
+
+
+            </div>
+          ))
+        ) : (
+          <p>No products found.</p>
+        )}
+      </div>
+
+
       <div className="bg-[#FDFBF7] mt-10">
-        <div className="mx-50   py-12">
+
+        <div className="mx-30   py-12">
+
+          <div className="mx-10">
+            <h3 className="text-3xl font-semibold mb-6  ">What Our Customers Are Saying</h3>
+            <p className="  text-xl mb-10">
+              Real feedback from people who love shopping with us.
+            </p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+
+
+              <div className="bg-[#F8F6F0]  p-6 flex flex-col gap-2 ">
+                <div className="flex gap-4">
+                  <img className="w-20 h-20 rounded-full" src="https://fireflyphotographysg.com/wp-content/uploads/2024/03/5-steps-to-mastering-the-perfect-linkedin-profile-picture.jpg" alt="" />
+                  <div className="flex flex-col gap-1">
+                    <span className="text-2xl">Sarah Johnson</span>
+                    <span className="text-lg">Verified Customer</span>
+
+                    <span className="flex"><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></span>
+                  </div>
+
+                </div>
+                <span className="text-xs mt-2 ml-1">02/03/2025</span>
+                <p className="text-xl"> "Bazarly has made my grocery shopping so much easier. Fast delivery and great quality products!"</p>
+              </div>
+
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+
+      <div className="bg-[#FDFBF7] mt-10">
+        <div className="mx-30   py-12">
           <div className="grid grid-cols-2 gap-8">
             <div className="mx-10">
-              <h3 className="text-2xl font-normal mb-6  ">Bazarly Subscription</h3>
-              <p className="  text-lg mb-10 ">
+              <h3 className="text-3xl font-semibold mb-6  ">Bazarly Subscription</h3>
+              <p className="  text-xl mb-10 ">
                 Enjoy exclusive benefits and personalized offers. We’re committed to providing you with the best shopping experience possible.
               </p>
               <div className="grid grid-cols-2">
 
-                <div className="   p-6 flex flex-col items-center justify-center ">
+                <div className="   p-6 flex flex-col items-center  ">
                   <span className=" text-5xl mb-4  ">
                     <SlBasket />
                   </span>
@@ -62,75 +199,8 @@ const Home = () => {
 
         </div>
       </div>
-      <div className="bg-[#FDFBF7] mt-10">
 
-        <div className="mx-30   py-12">
-
-          <div className="mx-10">
-            <h3 className="text-2xl font-normal mb-6  ">What Our Customers Are Saying</h3>
-            <p className="  text-lg mb-10 ">
-              Real feedback from people who love shopping with us.
-            </p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-
-              <div className="bg-[#F8F6F0] p-6 flex flex-col gap-4 ">
-                <div className="flex gap-4">
-                  <img className="w-20 h-20 rounded-full" src="https://fireflyphotographysg.com/wp-content/uploads/2024/03/5-steps-to-mastering-the-perfect-linkedin-profile-picture.jpg" alt="" />
-                  <div className="flex flex-col gap-1">
-                    <span className="text-xl">Sarah Johnson</span>
-                    <span>Verified Customer</span>
-                    <span className="flex"><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></span>
-                  </div>
-
-                </div>
-
-                <p>"Bazarly has made my grocery shopping so much easier. Fast delivery and great quality products!"</p>
-              </div>
-              <div className="bg-[#F8F6F0]  p-6 flex flex-col gap-4 ">
-                <div className="flex gap-4">
-                  <img className="w-20 h-20 rounded-full" src="https://fireflyphotographysg.com/wp-content/uploads/2024/03/5-steps-to-mastering-the-perfect-linkedin-profile-picture.jpg" alt="" />
-                  <div className="flex flex-col gap-1">
-                    <span className="text-xl">Sarah Johnson</span>
-                    <span>Verified Customer</span>
-                    <span className="flex"><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></span>
-                  </div>
-
-                </div>
-
-                <p>"Bazarly has made my grocery shopping so much easier. Fast delivery and great quality products!"</p>
-              </div>
-              <div className="bg-[#F8F6F0]  p-6 flex flex-col gap-4 ">
-                <div className="flex gap-4">
-                  <img className="w-20 h-20 rounded-full" src="https://fireflyphotographysg.com/wp-content/uploads/2024/03/5-steps-to-mastering-the-perfect-linkedin-profile-picture.jpg" alt="" />
-                  <div className="flex flex-col gap-1">
-                    <span className="text-xl">Sarah Johnson</span>
-                    <span>Verified Customer</span>
-                    <span className="flex"><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></span>
-                  </div>
-
-                </div>
-
-                <p>"Bazarly has made my grocery shopping so much easier. Fast delivery and great quality products!"</p>
-              </div>
-              <div className="bg-[#F8F6F0] p-6 flex flex-col gap-4 ">
-                <div className="flex gap-4">
-                  <img className="w-20 h-20 rounded-full" src="https://fireflyphotographysg.com/wp-content/uploads/2024/03/5-steps-to-mastering-the-perfect-linkedin-profile-picture.jpg" alt="" />
-                  <div className="flex flex-col gap-1">
-                    <span className="text-xl">Sarah Johnson</span>
-                    <span>Verified Customer</span>
-                    <span className="flex"><FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /></span>
-                  </div>
-
-                </div>
-
-                <p>"Bazarly has made my grocery shopping so much easier. Fast delivery and great quality products!"</p>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-      <div className="bg-[#FDFBF7] mt-10">
+      <div className="bg-[#F8F6F0] mt-10">
         <div className="mx-30 py-12 text-center">
           <h3 className="text-3xl text-centertext-2xl font-normal mb-6 ">
             Ready to Start Shopping?
@@ -140,16 +210,16 @@ const Home = () => {
           </p>
 
           <div className="flex justify-center">
-            <button className="bg-white border border-black px-14 py-3 cursor-pointer hover:bg-gray-100 ">
+            <button className="bg-white border text-lg border-black px-14 py-3 cursor-pointer hover:bg-gray-100 ">
               Get Started Today
             </button>
           </div>
         </div>
-</div>
-
       </div>
 
-      )
+    </div>
+
+  )
 }
 
-      export default Home
+export default Home
